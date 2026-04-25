b311bd8d6970fa7056363330247ba27d522abc95
Yesica Gonzalez <yesicagonzalezv99@gmail.com>
2026-04-25 15:08:42 -0500

    Delete src directory

--- src/mockdata/users.js
+++
@@ -1,4 +0,0 @@
-export const mockUsers = [
-  { id: 1, name: "Ana García", email: "ana@email.com", password: "123456" },
-  { id: 2, name: "Carlos López", email: "carlos@email.com", password: "123456" },
-];
